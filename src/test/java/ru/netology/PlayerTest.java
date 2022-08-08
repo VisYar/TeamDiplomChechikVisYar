6311f9a1f72475e6bc72d7d82f3c3d2042984368
Danila Kalay <ChechikLud@gmail.com>
2022-08-08 20:23:33 +0300

    new test and bag report commit

--- src/test/java/ru/netology/PlayerTest.java
+++ src/test/java/ru/netology/PlayerTest.java
@@ -18,6 +18,45 @@ public class PlayerTest {
         int actual = player.sumGenre(game.getGenre());
         assertEquals(expected, actual);
     }
+    @Test
+    public void outputRuntimeExceptionGameNotInstall() {
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+
+
+        Player player = new Player("Vasya");
+
+        assertThrows(RuntimeException.class, () -> {
+
+            player.play(game, 5);
+        });
+    }
+    @Test
+    public void mostPlayerByGenreGamePlayed() {
+        Player player = new Player("Dan");
+
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game2 = store.publishGame("F1", "Гонки");
+        Game game3 = store.publishGame("Цивилизация", "Стратегия");
+        Game game4 = store.publishGame("Ведьмак", "РПГ");
+
+        player.installGame(game);
+        player.installGame(game2);
+        player.installGame(game3);
+        player.installGame(game4);
+
+        player.play(game, 6);
+        player.play(game2, 5);
+        player.play(game3, 3);
+        player.play(game4, 7);
+
+
+        Game expected = game4;
+        Game actual = player.mostPlayerByGenre("РПГ");
+
+        assertEquals(expected, actual);
+    }
 
     // другие ваши тесты
 }
