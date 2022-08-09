c5e748d38cc08548bf2e17f03d361ed712d0cd40
Tatyana Dmitrieva <visyar76@gmail.com>
2022-08-09 13:45:55 +0300

    Divided asserts into different tests

--- src/test/java/ru/netology/GameStoreTest.java
+++ src/test/java/ru/netology/GameStoreTest.java
@@ -5,23 +5,28 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class GameStoreTest {
-    GameStore store = new GameStore();
-
-    Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
-    Game game1 = store.publishGame("Minecraft", "Симулятор");
-    Game game2 = store.publishGame("F1", "Гонки");
-    Game game3 = store.publishGame("Цивилизация", "Стратегия");
-    Game game4 = store.publishGame("Ведьмак", "РПГ");
 
     @Test
-    public void shouldAddGames() {
+    public void shouldAddOneGame() {
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
 
         assertTrue(store.containsGame(game));
-        assertTrue(store.containsGame(game1));
-        assertTrue(store.containsGame(game2));
-        assertTrue(store.containsGame(game3));
-        assertTrue(store.containsGame(game4));
-        assertFalse(store.containsGame(null));
     }
 
+    @Test
+    public void shouldContainsLastGames() {
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game1 = store.publishGame("Minecraft", "Симулятор");
+        Game game2 = store.publishGame("F1", "Гонки");
+
+        assertTrue(store.containsGame(game2));
+    }
+
+    @Test
+    public void shouldAddGameNotFound() {
+        GameStore store = new GameStore();
+        assertFalse(store.containsGame(null));
+    }
 }
